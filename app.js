fc0f4fcb97f2e210d7e3dcae85248d5d643d3495
Rajendraprasath <59873620+SSRaj001@users.noreply.github.com>
2020-11-12 16:33:29 +0530

    First Post Request successful

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql')
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 const path = require('path')
+
 
 const dbConnect = mysql.createConnection({
     host:'localhost',
@@ -20,20 +22,41 @@ dbConnect.connect(function(error){
 })
 
 app.use('/', express.static(__dirname + '/application'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+app.get("/",(req,res) =>{
+    var options = { 
+        root: path.join(__dirname) 
+    }; 
+    res.sendFile("application/index.html",options)
+    dbConnect.query("SELECT * from Location where pincode = '600001'",function(error,rows,fields){
+        //callback
+        if(!!error)
+            console.log('Error');
+        else 
+            console.log(rows);
+    });
+})
+app.get('/shplogin', function(req, res, next) { 
+    res.render('shplogin.ejs')
+    });
 
-// app.get("/",(req,res) =>{
-//     var options = { 
-//         root: path.join(__dirname) 
-//     }; 
-//     res.sendFile("application/index.html",options)
-//     dbConnect.query("SELECT * from Location where pincode = '600001'",function(error,rows,fields){
-//         //callback
-//         if(!!error)
-//             console.log('Error');
-//         else 
-//             console.log(rows);
-//     });
-// })
+app.post('/shplogin/signinpat',function(req,res,next){
+    const userDetails = req.body;
+    console.log(userDetails);
+    var str1 = "Select pPwd from patient where pID = '";
+    var str2 = "'"
+    var qry = str1.concat(userDetails.patientID);
+    var qry = qry.concat(str2);
+    dbConnect.query(qry,function(error,rows,fields){
+    if(!!error)
+        console.log('Error');
+    else 
+        console.log(rows[0].pPwd == userDetails.patientPwd); 
+    })
+});
+
 
 app.listen(port,() => {
     console.log("Port 3000 Accessed");
--- application/js/script.js
+++ application/js/script.js
@@ -6,71 +6,72 @@ function viewSignUp(){
     document.getElementById("signin").style.display = "none";
     document.getElementById("signup").style.display = "block";
 }
-document.getElementById("time").style.float= "right";
 
-function myTimer() {
-    let n =  new Date();
-    m = n.getMonth() + 1;
-    document.getElementById("date").innerHTML = n.getDate() + " / " + m + " / " + n.getFullYear();
-    document.getElementById("time").innerHTML = n.toLocaleTimeString()
-}
-var myVar = setInterval(myTimer, 1000);
+// document.getElementById("time").style.float= "right";
 
-function onCreatePresLoad(){
-    let n =  new Date();
-    m = n.getMonth() + 1;
-    document.getElementById("date1").innerHTML = n.getDate() + " / " + m + " / " + n.getFullYear();
-}
+// function myTimer() {
+//     let n =  new Date();
+//     m = n.getMonth() + 1;
+//     document.getElementById("date").innerHTML = n.getDate() + " / " + m + " / " + n.getFullYear();
+//     document.getElementById("time").innerHTML = n.toLocaleTimeString()
+// }
+// var myVar = setInterval(myTimer, 1000);
 
-function roomAllocation() {
-    let string = "Patient Alloacated Room No : "+ Math.floor((Math.random() * 10) + 1);
-    alert(string);
-}
+// function onCreatePresLoad(){
+//     let n =  new Date();
+//     m = n.getMonth() + 1;
+//     document.getElementById("date1").innerHTML = n.getDate() + " / " + m + " / " + n.getFullYear();
+// }
 
-function roomSearch() {
-    console.log("Came Here");
-    window.alert("Room details Found : ");
-}
+// function roomAllocation() {
+//     let string = "Patient Alloacated Room No : "+ Math.floor((Math.random() * 10) + 1);
+//     alert(string);
+// }
 
-function viewPresc(){
-    document.getElementById("supply").style.display = "none";
-    document.getElementById("order").style.display = "block";
-}
-function viewOrder(){
-    document.getElementById("order").style.display = "none";
-    document.getElementById("supply").style.display = "block";
-}
-function addMed(){
-    window.location.href = "addToStock.html";
-}
-function stockCancel(){
-    if(window.confirm("Cancel the Process ?")){
-        window.alert("Cancelled");
-        window.location.href = "pharma.html";
-    }
-}
-function addStock(){
-    if(window.confirm("Confirm?")){
-        window.alert("Added");
-        window.location.href = "pharma.html";
-    }
-}
-function returnToStock(){
-    window.location.href = "pharma.html";
-}
-function roomSearch(){
-    window.location.href = "roomSearch.html";
-}
-function returnToPres(){
-    window.location.href = "createPres.html";
-}
-function presCancel(){
-    if(window.confirm("Cancel the Process ?")){
-        window.alert("Cancelled");
-        window.location.href = "createPres.html";
-    }
-}
+// function roomSearch() {
+//     console.log("Came Here");
+//     window.alert("Room details Found : ");
+// }
 
-function addPres(){
-    window.location.href = "createPres.html";
-}
+// function viewPresc(){
+//     document.getElementById("supply").style.display = "none";
+//     document.getElementById("order").style.display = "block";
+// }
+// function viewOrder(){
+//     document.getElementById("order").style.display = "none";
+//     document.getElementById("supply").style.display = "block";
+// }
+// function addMed(){
+//     window.location.href = "addToStock.html";
+// }
+// function stockCancel(){
+//     if(window.confirm("Cancel the Process ?")){
+//         window.alert("Cancelled");
+//         window.location.href = "pharma.html";
+//     }
+// }
+// function addStock(){
+//     if(window.confirm("Confirm?")){
+//         window.alert("Added");
+//         window.location.href = "pharma.html";
+//     }
+// }
+// function returnToStock(){
+//     window.location.href = "pharma.html";
+// }
+// function roomSearch(){
+//     window.location.href = "roomSearch.html";
+// }
+// function returnToPres(){
+//     window.location.href = "createPres.html";
+// }
+// function presCancel(){
+//     if(window.confirm("Cancel the Process ?")){
+//         window.alert("Cancelled");
+//         window.location.href = "createPres.html";
+//     }
+// }
+
+// function addPres(){
+//     window.location.href = "createPres.html";
+// }
